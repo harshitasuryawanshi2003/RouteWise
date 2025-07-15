b8b8c011d793ec58f3812b879605537afb13f5e2
harshitasuryawanshi2003 <harshitasuryawanshi2003@gmail.com>
2025-07-15 22:50:57 +0530

    Updated Protected Routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SignupForm from './components/Auth/SignupForm';
 import AdminDashboard from './components/Admin/AdminDashboard';
 import CollectorDashboard from './components/Collector/CollectorDashboard';
 import CitizenDashboard from './components/Citizen/CitizenDashboard';
+import ProtectedRoute from './components/Common/ProtectedRoute';
 
 function App() {
   return (
@@ -12,9 +13,21 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginForm />} />
           <Route path="/signup" element={<SignupForm />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/collector" element={<CollectorDashboard/>}/>
-          <Route path="/citizen" element={<CitizenDashboard/>}/>
+          <Route path="/admin"  element={
+            <ProtectedRoute allowedRoles={["Admin"]}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="/collector"   element={
+            <ProtectedRoute allowedRoles={["Collector"]}>
+              <CollectorDashboard />
+            </ProtectedRoute>
+          }/>
+          <Route path="/citizen"   element={
+            <ProtectedRoute allowedRoles={["Citizen"]}>
+              <CitizenDashboard />
+            </ProtectedRoute>
+          }/>
           <Route path="/" element={<Navigate to="/login" replace />} />
         </Routes>
       </div>
